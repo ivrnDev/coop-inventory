5f3a0e1f0e7383d91871537436d65b3d2584f7fc
Ivan Ren <villamora.ivanren.manguiat@gmail.com>
2023-09-19 07:27:51 -0700

    Fix Update Function

--- src/api/products/products.controller.js
+++ src/api/products/products.controller.js
@@ -7,12 +7,12 @@ const {
 
 const controller = {
   createProduct: async (req, res) => {
-    try {      
+    try {
       if (!req.file) {
         return res.status(400).json({ error: 'No file uploaded, image is required' });
       }
       const imagePath = req.file.filename;
-      const result = await createProduct(req.body, imagePath) 
+      const result = await createProduct(req.body, imagePath)
       return res.status(201).json(result)
     } catch (error) {
       if (error) return res.status(500).json(error)
@@ -29,35 +29,21 @@ const controller = {
   updateProducts: async (req, res) => {
     try {
       const imagePath = req.file.filename;
-      const result = await updateProducts(req.body, req.params.id, imagePath) 
+      const result = await updateProducts(req.body, req.params.id, imagePath)
       return res.status(200).json(result)
     } catch (error) {
       return res.status(400).json(error);
-    } 
+    }
   },
-  deleteProduct: (req, res) => {
-    deleteProduct(req.params.id, (error, results) => {
-      if (error) return res.status(400).json({ message: error });
-      return res.status(200).json({ message: `Successfully deleted product with an ID of ${req.params.id}`, results });
-    })
+  deleteProduct: async (req, res) => {
+    try {
+      const result = await deleteProduct(req.params.id);
+      res.status(200).json({ message: `Successfully deleted product with an ID of ${req.params.id}`, result });
+    } catch (error) {
+      return res.status(400).json({ message: error });
+    }
   }
-
-
-
-
-
-
-
-
-
-
-
-  // getProductDetails: (req, res) => {
-  //   getProductDetails((req.body, (error, results) => {
-  //     if(error) return res.status(400).json({message: error});
-  //     return res.status(200).json({message: "Fetched the data", data: results})
-  //   }))
-  // }
 }
+
 
 module.exports = controller
--- src/api/products/products.service.js
+++ src/api/products/products.service.js
@@ -34,41 +34,50 @@ const service = {
       pool.execute('SELECT * FROM products WHERE id = ?', [id], (error, result) => {
         if (error) return reject({ message: "Internal Server Error", error: error })
         if (result.length === 0) return reject({ message: `There is no products with an ID of ${id}` })
-        
+
+        const image = result[0].images;
+
         pool.execute('UPDATE products SET name = ?, price = ?, stocks = ?, images = ? WHERE id = ?', [
           name, price, stocks, imagePath, id
         ], (error, result) => {
-          if (error) return reject({message: "Failed to update data", error: error});
-          return resolve({message: "Successfully Updated the data", result: result});
+          if (error) return reject({ message: "Failed to update data", error: error });
+          if (imagePath) {
+            fs.unlink(`./images/${image}`, (err) => {
+              if (err) {
+                console.error('Error deleting file:', err);
+              } else {
+                console.log('File deleted successfully');
+              }
+            })
+          }
+          return resolve({ message: "Successfully Updated the data", result: result });
         })
       })
 
     })
 
   },
-  deleteProduct: (id, callback) => {
-
-    pool.execute('SELECT images FROM products WHERE id = ?', [id], (error, results) => {
-      if (error) return callback(error)
-      if (results.length === 0) return callback(`There is no products with an ID of ${id}`);
-      const imagePath = results[0].images;
-      pool.execute('DELETE FROM products WHERE id = ?', [id], (error, results) => {
-        if (error) return callback(error)
-        if (imagePath) {
-          // const filePath = path.join(__dirname, 'images', imagePath);
-          fs.unlink(`./images/${imagePath}`, (err) => {
-            if (err) {
-              console.error('Error deleting file:', err);
-            } else {
-              console.log('File deleted successfully');
-            }
-          });
-        }
-
-        return callback(null, results)
+  deleteProduct: (id) => {
+    return new Promise((resolve, reject) => {
+      pool.execute('SELECT images FROM products WHERE id = ?', [id], (error, results) => {
+        if (error) return reject(error)
+        if (results.length === 0) return reject(`There is no products with an ID of ${id}`);
+        const imagePath = results[0].images;
+        pool.execute('DELETE FROM products WHERE id = ?', [id], (error, results) => {
+          if (error) return reject(error)
+          if (imagePath) {
+            fs.unlink(`./images/${imagePath}`, (err) => {
+              if (err) {
+                console.error('Error deleting file:', err);
+              } else {
+                console.log('File deleted successfully');
+              }
+            });
+          }
+          return resolve(results)
+        })
       })
     })
-
   }
 };
 
