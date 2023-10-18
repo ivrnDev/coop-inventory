326cb6eab531d1c0d2e60b39fea56655c465ecff
Ivan Ren <villamora.ivanren.manguiat@gmail.com>
2023-10-18 01:22:22 -0700

    updating media query breakpoint

--- qcu-coop/tailwind.config.ts
+++ qcu-coop/tailwind.config.ts
@@ -15,6 +15,13 @@ module.exports = {
         "2xl": "1400px",
       },
     },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+      "2xl": "1536px",
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
