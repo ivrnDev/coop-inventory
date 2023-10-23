5c059196f3c3bbe59deb51f49ab316a7163c96a4
Ivan Ren <villamora.ivanren.manguiat@gmail.com>
2023-10-23 07:26:29 -0700

    set selected filters

--- qcu-coop/components/admin/products/render/transactions/filter.tsx
+++ qcu-coop/components/admin/products/render/transactions/filter.tsx
@@ -1,13 +1,15 @@
 "use client";
-
 import { Button } from "@/components/ui/button";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import classNames from "classnames";
+import { useState } from "react";
 
 const TransactionFilter = () => {
   const searchParams = useSearchParams();
   const parameters = searchParams.get("id");
   const router = useRouter();
   const pathname = usePathname();
+  const [selectedFilter, setSelectedFilter] = useState("");
 
   const handleFilter = (filter: string) => {
     const params = new URLSearchParams({ filter });
@@ -17,35 +19,44 @@ const TransactionFilter = () => {
     }
 
     router.push(`${pathname}?${params.toString()}`);
+    setSelectedFilter(filter);
   };
 
   return (
     <div className="flex space-x-6">
       <Button
-        variant="default"
-        className="rounded-xl"
         onClick={() => handleFilter("all")}
+        className={classNames({
+          "rounded-xl": true,
+          "bg-green-600": selectedFilter === "all",
+        })}
       >
         All
       </Button>
       <Button
-        variant="default"
-        className="rounded-xl"
         onClick={() => handleFilter("pending")}
+        className={classNames({
+          "rounded-xl": true,
+          "bg-green-600": selectedFilter === "pending",
+        })}
       >
         Pending
       </Button>
       <Button
-        variant="default"
-        className="rounded-xl"
         onClick={() => handleFilter("completed")}
+        className={classNames({
+          "rounded-xl": true,
+          "bg-green-600": selectedFilter === "completed",
+        })}
       >
         Completed
       </Button>
       <Button
-        variant="default"
-        className="rounded-xl"
         onClick={() => handleFilter("cancelled")}
+        className={classNames({
+          "rounded-xl": true,
+          "bg-green-600": selectedFilter === "cancelled",
+        })}
       >
         Cancelled
       </Button>
