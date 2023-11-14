a4e71df501fbed9b6a871ce167d0b36daab071b1
Ivan Ren <villamora.ivanren.manguiat@gmail.com>
2023-11-14 12:52:47 +0800

    Fixed admin navbar

--- qcu-coop/components/header/AdminNavbar.tsx
+++ qcu-coop/components/header/AdminNavbar.tsx
@@ -1,5 +1,39 @@
 import Link from "next/link";
 import Image from "next/image";
+
+const link = [
+  {
+    name: "Dashboard",
+    href: "/admin/dashboard",
+    image: "/icons/dashboard-icon.png",
+  },
+  {
+    name: "Orders",
+    href: "/admin/orders?filter=all",
+    image: "/icons/orders-icon.png",
+  },
+  {
+    name: "Products",
+    href: "/admin/products",
+    image: "/icons/products-icon.png",
+  },
+  {
+    name: "Analytics",
+    href: "/admin/analytics",
+    image: "/icons/analytics-icon.png",
+  },
+  {
+    name: "Accounts",
+    href: "/admin/accounts",
+    image: "/icons/accounts-icon.png",
+  },
+  {
+    name: "Logout",
+    href: "/",
+    image: "/icons/logout-icon.png",
+  },
+];
+
 const AdminNavBar = () => {
   return (
     <nav
@@ -20,102 +54,24 @@ const AdminNavBar = () => {
         </h1>
       </div>
       <div className="flex md:flex-col space-x-10 md:space-x-0 justify-center items-center md:items-start md:space-y-6 p-2 w-[inherit]">
-        <Link href="/admin/dashboard">
-          <div className="flex flex-row-reverse gap-3 items-center">
-            <p className="hidden md:inline font-bold text-lg uppercase">
-              Dashboard
-            </p>
-            <div className="relative w-10 h-10">
-              <Image
-                src="/icons/dashboard-icon.png"
-                alt="dashboard-icon"
-                sizes="(min-width: 2rem)"
-                fill
-                className="hover:animate-bounce md:hover:animate-none"
-              />
+        {link.map((value, index) => (
+          <Link href={value.href} key={index}>
+            <div className="hidden md:flex flex-row-reverse gap-3 items-center">
+              <p className="hidden md:inline font-bold text-lg uppercase">
+                {value.name}
+              </p>
+              <div className="relative w-10 h-10">
+                <Image
+                  src={value.image}
+                  alt={value.name}
+                  sizes="(min-width: 2rem)"
+                  fill
+                  className="hover:animate-bounce md:hover:animate-none"
+                />
+              </div>
             </div>
-          </div>
-        </Link>
-        <Link href="/admin/orders?filter=all">
-          <div className="flex flex-row-reverse gap-3 items-center ">
-            <p className="hidden md:inline font-bold text-lg uppercase">
-              Orders
-            </p>
-            <div className="relative w-10 h-10">
-              <Image
-                src="/icons/orders-icon.png"
-                alt="orders-icon"
-                sizes="(min-width: 2rem)"
-                fill
-                className="hover:animate-bounce md:hover:animate-none"
-              />
-            </div>
-          </div>
-        </Link>
-        <Link href="/admin/products">
-          <div className="flex flex-row-reverse gap-3 items-center">
-            <p className="hidden md:inline font-bold text-lg uppercase">
-              Products
-            </p>
-            <div className="relative w-10 h-10">
-              <Image
-                src="/icons/products-icon.png"
-                alt="products-icon"
-                sizes="(min-width: 2rem)"
-                fill
-                className="hover:animate-bounce md:hover:animate-none"
-              />
-            </div>
-          </div>
-        </Link>
-        <Link href="/admin/analytics">
-          <div className="flex flex-row-reverse gap-3 items-center">
-            <p className="hidden md:inline font-bold text-lg uppercase">
-              Analytics
-            </p>
-            <div className="relative w-10 h-10 ">
-              <Image
-                src="/icons/analytics-icon.png"
-                alt="analytics-icon"
-                sizes="(min-width: 2rem)"
-                fill
-                className="hover:animate-bounce md:hover:animate-none"
-              />
-            </div>
-          </div>
-        </Link>
-        <Link href="/admin/accounts">
-          <div className="flex flex-row-reverse gap-3 items-center">
-            <p className="hidden md:inline font-bold text-lg uppercase">
-              Accounts
-            </p>
-            <div className="relative w-10 h-10">
-              <Image
-                src="/icons/accounts-icon.png"
-                alt="accounts-icon"
-                sizes="(min-width: 2rem)"
-                fill
-                className="hover:animate-bounce md:hover:animate-none"
-              />
-            </div>
-          </div>
-        </Link>
-        <Link href="/">
-          <div className="hidden md:flex flex-row-reverse gap-3 items-center">
-            <p className="hidden md:inline font-bold text-lg uppercase">
-              Logout
-            </p>
-            <div className="relative w-10 h-10">
-              <Image
-                src="/icons/logout-icon.png"
-                alt="logout-icon"
-                sizes="(min-width: 2rem)"
-                fill
-                className="hover:animate-bounce md:hover:animate-none"
-              />
-            </div>
-          </div>
-        </Link>
+          </Link>
+        ))}
       </div>
     </nav>
   );
