e9b46073b232d4b42ed34d92c84fff6605c932c4
Ivan Ren <villamora.ivanren.manguiat@gmail.com>
2023-11-20 00:18:25 +0800

    Remove unnecessary jsx

--- qcu-coop/app/(user)/page.tsx
+++ qcu-coop/app/(user)/page.tsx
@@ -57,35 +57,6 @@ const Home = async ({ searchParams }: Props) => {
           id="products-container"
           className="mt-5 grid grid-cols-3 md:grid-cols-6 gap-4 p-4"
         >
-          {/* {products?.length > 0 &&
-            products.map((product, index) => (
-              <Link
-                href={`
-            /products/${product.product_id}`}
-                key={index}
-                className="h-fit "
-              >
-                <div className="bg-white h-64 w-full shadow-xl rounded-lg flex flex-col p-2 hover:opacity-80 flex-1 md:h-72">
-                  <div className="relative w-full h-32 overflow-hidden rounded-md border border-black">
-                    <Image
-                      src={`data:image/png;base64,${product.display_image}`}
-                      alt={product.product_name}
-                      fill
-                      className="object-contain"
-                    />
-                  </div>
-                  <div className="mt-3 px-1">
-                    <h1 className="text-sm">{product.display_name}</h1>
-                    <p className="text-custom-orange mt-2 text-sm">
-                      ₱ {product.display_price}
-                    </p>
-                    <p className="mt-2 text-sm opacity-75 max-w-xs text-ellipsis overflow-hidden whitespace-nowrap">
-                      {product.product_stocks} stocks
-                    </p>
-                  </div>
-                </div>
-              </Link>
-            ))} */}
             <RenderProducts products={products}/>
         </div>
       </section>
