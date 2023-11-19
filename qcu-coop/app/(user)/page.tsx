706df6c453ccbb0024d7a4c4725e9315464c21c9
Ivan Ren <villamora.ivanren.manguiat@gmail.com>
2023-11-19 23:13:12 +0800

    Add banner

--- qcu-coop/app/(user)/page.tsx
+++ qcu-coop/app/(user)/page.tsx
@@ -2,8 +2,10 @@ import Banner from "@/components/user/carousel/Banner";
 import CategoriesCarousel from "@/components/user/carousel/Categories";
 import FeaturedCarousel from "@/components/user/carousel/Featured";
 import RenderProducts from "@/components/user/render/Products";
+import { getAllBanners } from "@/lib/api/banner";
 import { getAllCategories } from "@/lib/api/categories";
 import {  getAllProducts, getProductByFeatured } from "@/lib/api/products";
+import { Banners } from "@/types/banners/banners";
 import { Categories, Featured, Products } from "@/types/products/products";
 
 type Props = {
@@ -15,22 +17,23 @@ const Home = async ({ searchParams }: Props) => {
   const products: Products[] = await getAllProducts(null, true);
   const featured: Featured[] = await getProductByFeatured();
   const categories: Categories[] = await getAllCategories();
+   const banners: Banners[] = await getAllBanners();
   return (
     <>
       <section className="min-h-user-main-mobile md:min-h-user-main mb-9">
         <div id="home-container" className="flex flex-col h-full">
-          <div className=" h-40 md:h-96 bg-green-500">
-            <Banner />
+          <div className=" h-40 md:h-96">
+            <Banner banners={banners} />
           </div>
           <div className="bg-featured-background flex flex-col items-center justify-center p-5 h-64 md:h-96">
             <div
               id="featured-header"
               className="flex space-x-2 items-center text-xl md:text-4xl font-extrabold"
             >
-              <h1 className="bg-white drop-shadow-xl shadow-xl rounded-sm text-[#F3C72E] w-fit h-fit p-1">
-                NEW
+              <h1 className="bg-white drop-shadow-xl shadow-xl rounded-sm text-[#F3C72E] w-fit h-fit p-1 select-none">
+                FEATURED
               </h1>
-              <p className="text-white">ARRIVALS</p>
+              <p className="text-white select-none">PRODUCTS</p>
             </div>
             <div
               id="arrivals-container"
--- qcu-coop/components/user/carousel/Banner.tsx
+++ qcu-coop/components/user/carousel/Banner.tsx
@@ -1,8 +1,31 @@
-const Banner = () => {
+"use client";
+import { Banners } from "@/types/banners/banners";
+import Image from "next/image";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+type Props = {
+  banners: Banners[];
+};
+const Banner = ({ banners }: Props) => {
   return (
-    <>
-      <div className="">Banner</div>
-    </>
+    <Carousel
+      autoPlay={true}
+      infiniteLoop={true}
+      showArrows={false}
+      interval={4000}
+    >
+      {banners &&
+        banners.map((banner, index) => (
+          <div key={index} className="w-full h-40 md:h-96 overflow-hidden">
+            <Image
+              src={`data:image/png;base64,${banner.banner_image}`}
+              alt={`banner${index}`}
+              fill
+              className="object-cover"
+            />
+          </div>
+        ))}
+    </Carousel>
   );
 };
 
