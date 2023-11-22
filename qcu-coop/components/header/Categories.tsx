37e2f28487b26ec63c7acfee0ffae05c558bfab0
Ivan Ren <villamora.ivanren.manguiat@gmail.com>
2023-11-22 16:50:05 +0800

    Fix categories desktop view

--- qcu-coop/components/header/Categories.tsx
+++ qcu-coop/components/header/Categories.tsx
@@ -11,13 +11,14 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../ui/select";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import classNames from "classnames";
 
 const CategoriesSidebar = () => {
   const router = useRouter();
+  const params = useSearchParams();
   const [categories, setCategories] = useState<Categories[]>([]);
   useEffect(() => {
     const getCategories = async () => {
@@ -42,7 +43,7 @@ const CategoriesSidebar = () => {
 
   return (
     <>
-      <aside className="mt-user-header-mobile h-fit w-20 fixed top-2 left-6 z-50 md:mt-user-header md:top-10 md:w-fit md:left-10 md:h-64 md:overflow-y-auto">
+      <aside className="mt-user-header-mobile h-fit w-20 fixed top-2 left-6 z-50 md:mt-user-header md:top-10 md:w-fit md:left-10 md:h-96 ">
         <Controller
           name="category_name"
           control={control}
@@ -64,7 +65,10 @@ const CategoriesSidebar = () => {
                     {!categories && <SelectLabel>No Categories</SelectLabel>}
                     <SelectItem value="all">All</SelectItem>
                     {categories?.map((category, index) => (
-                      <SelectItem key={index} value={String(category.category_name)}>
+                      <SelectItem
+                        key={index}
+                        value={String(category.category_name)}
+                      >
                         <p className="capitalize">{category.category_name}</p>
                       </SelectItem>
                     ))}
@@ -74,14 +78,17 @@ const CategoriesSidebar = () => {
             </>
           )}
         />
-        <h1 className="font-bold text-4xl hidden mb-3 md:block">Categories</h1>
+        <h1 className="font-bold text-3xl hidden mb-3 md:block md:fixed">
+          Categories
+        </h1>
         {categories && categories.length > 0 ? (
-          <ul className="w-full text-black font-semibold mt-3 space-y-3 hidden md:block">
+          <ul className="hidden md:block w-44 text-black font-semibold mt-14 space-y-3 h-96 overflow-y-auto">
             <li>
               <Link
                 href={`/products`}
                 className={classNames({
-                  "w-fit hover:text-blue-500 text-lg": true,
+                  "w-fit text-lg": true,
+                  "text-blue-600": params.get("category") === null,
                 })}
               >
                 All Products
@@ -93,7 +100,9 @@ const CategoriesSidebar = () => {
                   key={index}
                   href={`/products?category=${category.category_name}`}
                   className={classNames({
-                    "w-fit hover:text-blue-500 text-lg": true,
+                    "w-fit text-lg": true,
+                    "text-blue-600":
+                      params.get("category") === category.category_name,
                   })}
                 >
                   {category.category_name}
@@ -102,7 +111,9 @@ const CategoriesSidebar = () => {
             ))}
           </ul>
         ) : (
-          <p className="text-center text-lg mt-4 hidden md:block">No available categories</p>
+          <p className="text-center text-lg mt-10 hidden md:block">
+            No available categories
+          </p>
         )}
       </aside>
     </>
