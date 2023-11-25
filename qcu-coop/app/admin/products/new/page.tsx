bc6a4abfa0ca51f433bdf3a19f2ec7a231767852
Ivan Ren <villamora.ivanren.manguiat@gmail.com>
2023-11-25 14:42:13 +0800

    fix design

--- qcu-coop/app/admin/products/new/page.tsx
+++ qcu-coop/app/admin/products/new/page.tsx
@@ -1,12 +1,14 @@
 import CreateProductForm from "@/components/admin/products/forms/Createproduct";
 const CreateProductPage = () => {
   return (
-    <section className="bg-red-300 relative w-full h-[500px] rounded-md flex items-center border border-solid border-black overflow-hidden">
-      <div className="absolute bg-header-admin w-[100%] h-10 top-0 left-0 justify-center text-white text-[1.2rem] rounded-t-md flex items-center font-bold">
-        CREATE PRODUCT
-      </div>
-      <div className="bg-white w-full h-[calc(100%-2.5rem)] mt-10 oveflow-hidden py-5 px-9">
-        <CreateProductForm />
+    <section className="h-admin-main p-10 overflow-auto">
+      <div className="h-full border-black border rounded-md bg-white overflow-y-auto">
+        <div className="bg-header-admin w-[100%] h-10 font-bold text-xl flex justify-center items-center text-white">
+          CREATE PRODUCT
+        </div>
+        <div className="bg-white w-full h-[calc(100%-2.5rem)] mt-15 oveflow-hidden py-5 px-9">
+          <CreateProductForm />
+        </div>
       </div>
     </section>
   );
